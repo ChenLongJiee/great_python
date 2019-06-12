49e8280b193fa51270e1ea4c5d2767919dbc435c
Chanlongjie <51744793+Chanlongjie@users.noreply.github.com>
2019-06-12 23:51:07 +0800

    Add files via upload

---
+++ day4_work.py
@@ -0,0 +1,10 @@
+grade = int(input("小明考试考了"))
+if grade == 100:
+    print("爸爸奖励他一辆自行车")
+elif grade >= 90:
+    print("妈妈奖励他一部MP4")
+elif grade >= 60:
+    print("妈妈给他买了参考书")
+    print("下次继续努力呀！")
+else:
+    print("今天小明家一直到深夜没有平静。。。。")
